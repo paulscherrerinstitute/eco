99abac517b585ff5bcf891ba7b19f806b52bd303
gac-alvra (Resp.Milne Christopher) <gac-alvra@saresa-cons-03.psi.ch>
2018-03-01 18:32:56 +0100

    modified dcm

--- xoptics/dcm.py
+++ xoptics/dcm.py
@@ -16,7 +16,6 @@ class Double_Crystal_Mono:
      self.moving = PV(Id+':MOVING')
      self._stop = PV(Id +':STOP.PROC')
 
-<<<<<<< HEAD
     def changeTo(self,value):
      self.energy_sp.put(value)
       
@@ -34,15 +33,3 @@ class Double_Crystal_Mono:
      inmotion = int(self.moving.get())
      return inmotion   
     
-=======
-        self.th = MotorRecord(Id+':RX12')
-        self.x = MotorRecord(Id+':TX12')
-        self.gap = MotorRecord(Id+':T2')
-        self.roll1 = MotorRecord(Id+':RZ1')
-        self.roll2 = MotorRecord(Id+':RZ2')
-        self.pitch2 = MotorRecord(Id+':RX2')
-
-        self.energy = PV(Id+':ENERGY').value
-#		self.set_energy = PV(Id+':ENERGY_SP').value
-		
->>>>>>> 1e1a12d037b38eb4dd93c24a0eb6ee6a55ab6548
