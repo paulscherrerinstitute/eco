311c5453b83d1113a33664db822eaba607a21b11
gac-alvra (Resp.Milne Christopher) <gac-alvra@saresa-cons-03.psi.ch>
2018-03-07 00:45:28 +0100

    fixed stationMessage

--- utilities/newMsgPV.py
+++ utilities/newMsgPV.py
@@ -2,16 +2,19 @@
 from epics import PV
 import datetime
 import sys
-def stationMessage(station,message=''):   
-    msg=message
-    date_formatted=datetime.datetime.strftime(datetime.datetime.now(), "%a %d-%b-%Y %H:%M:%S")
-    mscroll=PV('SF-OP:'+str(station)+'-MSG:OP-MSCROLL.PROC')  
-    mscroll.value=1
-    msg1=PV('SF-OP:'+str(station)+'-MSG:OP-MSG1')  
-    msg1.value = msg.encode()
-    date1=PV('SF-OP:'+str(station)+'-MSG:OP-DATE1')
-    date1.value = date_formatted.encode()
-    msg_PV.disconnect()
-    msg1.disconnect()
-    date1.disconnect()
-
+class stationMessage:
+    def __init__(self,station):
+       self._BL = station
+       
+    def post(self,message):
+    	stationStr = self._BL
+    	msg=message
+    	date_formatted=datetime.datetime.strftime(datetime.datetime.now(), "%a %d-%b-%Y %H:%M:%S")
+    	mscroll=PV('SF-OP:'+str(stationStr)+'-MSG:OP-MSCROLL.PROC')  
+    	mscroll.value=1
+    	msg1=PV('SF-OP:'+str(stationStr)+'-MSG:OP-MSG1')  
+    	msg1.value = msg.encode()
+    	date1=PV('SF-OP:'+str(stationStr)+'-MSG:OP-DATE1')
+    	date1.value = date_formatted.encode()
+    	msg1.disconnect()
+    	date1.disconnect()
