9fc37343503d7244a7b80e861cf48a831cb8f8ab
Henrik Lemke <henrik.lemke@psi.ch>
2018-04-12 10:56:49 +0200

    additional materials

--- xsim/materials.py
+++ xsim/materials.py
@@ -33,10 +33,17 @@ crystal['Ge'] = xu.materials.Ge
 crystal['GaAs'] = xu.materials.GaAs
 crystal['Al'] = xu.materials.Al
 crystal['Diamond'] = xu.materials.C
+crystal['Be'] = xu.materials.material.Crystal("Be", \
+        xu.materials.spacegrouplattice.SGLattice(\
+        194, 2.2858, 3.5843, atoms=[xu.materials.elements.Be, ], \
+        pos=['2c', ]))
 
 amorphous['B4C'] = xu.materials.material.Amorphous('B4C',2520,[('B',4),('C',1)])
 amorphous['Mo'] = xu.materials.material.Amorphous('Mo',10220,[('Mo',1)])
 amorphous['polyimide'] = xu.materials.material.Amorphous('polyimide',1430,[('C',22),('H',10),('N',2),('O',5)])
+amorphous['mylar'] = xu.materials.material.Amorphous('mylar',1400,[('C',10),('H',8),('O',4)])
+amorphous['polycarbonate'] = xu.materials.material.Amorphous('polycarbonate',1200,[('C',16),('H',14),('O',3)])
+amorphous['Si3N4'] = xu.materials.material.Amorphous('Silicon nitride',3440,[('Si',3),('N',4)])
 amorphous['air'] = xu.materials.material.Amorphous('air',1000,[('N',1.562),('O',.42),('C',.0003),('Ar',.0094)])
 
 
@@ -73,6 +80,8 @@ class Gas(xu.materials.material.Amorphous):
         return w
 
 gas['air'] = Gas('air',molecule_size=1.9917,atoms=[('N',1.562),('O',.42),('C',.0003),('Ar',.0094)])
+gas['He'] = Gas('He',molecule_size=1,atoms=[('He',1)])
+gas['N'] = Gas('He',molecule_size=2,atoms=[('N',1)])
 
 
 
