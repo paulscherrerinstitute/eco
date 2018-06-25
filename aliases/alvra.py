32a854da21fcb1c78a8b83707f4bc3579af597ab
Chris Milne <cjmilne@gmail.com>
2018-06-25 23:45:11 +0200

    one more try

--- aliases/alvra.py
+++ aliases/alvra.py
@@ -216,7 +216,7 @@ aliases = {
                 'desc' : 'Experiment laser phase shifter (Globi)',
                 'eco_type' : 'devices_general.alvratiming.PhaseShifterAramis'},
         'SLAAR11-LMOT' : {
-                'alias' : 'LasExp',
+                'alias' : 'laser',
                 'z_und' : 127,
                 'desc' : 'Experiment laser optics',
                 'eco_type' : 'loptics.alvra_experiment.Laser_Exp'},
--- loptics/alvra_experiment.py
+++ loptics/alvra_experiment.py
@@ -1,46 +1,43 @@
 from ..devices_general.motors import MotorRecord
 from ..devices_general.smaract import SmarActRecord
-
 from epics import PV
 from ..devices_general.delay_stage import DelayStage
-#from ..devices_general.user_to_motor import User_to_motor
 
 class Laser_Exp:
 	def __init__(self,Id):
 		self.Id = Id
 
         #PALM delay stages 
-        try:
+		try:
 			self.palm_delay = MotorRecord(self.Id+'-M423:MOT')
 			self.palm_delayTime = DelayStage(self.palm_delay)
 		except:
-            print('No PALM delay line')
-            pass
+			print('No PALM delay line')
+			pass
         
-        try:
+		try:
 			self.palmEO_delay = MotorRecord(self.Id+'-M422:MOT')
 			self.palmEO_delayTime = DelayStage(self.palmEO_delay)
 		except:
-            print('No PALM-EO sampiing delay line')
-            pass
+			print('No PALM-EO sampiing delay line')
+			pass
 
         #PSEN delay stages
-        try:
+		try:
 			self.psen_delay = MotorRecord(self.Id+'-M424:MOT')
 			self.psen_delayTime = DelayStage(self.psen_delay)
 		except:
-            print('No PSEN delay line')
-            pass
+			print('No PSEN delay line')
+			pass
 
 	def get_adjustable_positions_str(self):
-        ostr = '*****Motor positions******\n'
+		ostr = '*****Motor positions******\n'
 
-        for tkey,item in self.__dict__.items():
+		for tkey,item in self.__dict__.items():
             if hasattr(item,'get_current_value'):
                 pos = item.get_current_value()
                 ostr += '  ' + tkey.ljust(10) + ' : % 14g\n'%pos
-        return ostr
+		return ostr
 
 	def __repr__(self):
-        return self.get_adjustable_positions_str()
-        
+		return self.get_adjustable_positions_str()
