3bad22759dfac59748814e9cf2fa520ef449101f
Chris Milne <cjmilne@gmail.com>
2018-06-25 21:58:57 +0200

    delay lines

--- loptics/alvra_experiment.py
+++ loptics/alvra_experiment.py
@@ -6,29 +6,8 @@ from ..devices_general.delay_stage import DelayStage
 from ..devices_general.user_to_motor import User_to_motor
 
 class Laser_Exp:
-    def __init__(self,Id):
+	def __init__(self,Id):
 		self.Id = Id
-
-
-
-#         try:
-#             self.lensx = MotorRecord('SARES20-EXP:MOT_DIODE')
-#         except:
-#             print('No owis lens x motor')
-#             pass
-        
-        #Waveplate and Delay stage
-#         self.wp = MotorRecord(Id+'-M534:MOT')
-  
-#         self._pump_delayStg = MotorRecord(self.Id+'-M521:MOTOR_1')
-#         self.pump_delay = DelayStage(self._pump_delayStg)
-
-        #LAM delay stages
-#         self._lam_delayStg_Smar = SmarActRecord('SLAAR21-LMTS-LAM11')
-#         self.lam_delay_Smar = DelayStage(self._lam_delayStg_Smar)
-
-#         self._lam_delayStg = MotorRecord(self.Id+'-M548:MOT')
-#         self.lam_delay = DelayStage(self._lam_delayStg)
 
         #PALM delay stages 
 		self.palm_delay = MotorRecord(self.Id+'-M423:MOT')
@@ -41,54 +20,7 @@ class Laser_Exp:
 		self.psen_delay = MotorRecord(self.Id+'-M424:MOT')
 		self.psen_delayTime = DelayStage(self.psen_delay)
 
-        #SmarAct ID
-#         self.IdSA = 'SARES23'
-
-        ### Mirrors used in the expeirment ###
-#         try:
-#             self.eos_rot = SmarActRecord(self.IdSA+'-ESB18')
-#         except:
-#             print('No Smaract EOSrot')
-#             pass
-# 
-#         try:
-#             self.eos_gonio = SmarActRecord(self.IdSA+'-ESB3')
-#         except:
-#             print('No Smaract EOSGonio')
-#             pass
-# 
-#         try:
-#             self._pump_rot = SmarActRecord(self.IdSA+'-ESB16')
-#             self.pump_rot = User_to_motor(self._pump_rot,180./35.7,0.)
-#         except:
-#             print('No Smaract THzrot')
-#             pass
-# 
-#         try:
-#             self.pump_gonio = SmarActRecord(self.IdSA+'-ESB2')
-#         except:
-#             print('No Smaract THzGonio')
-#             pass
-#         
-#         try:
-#             self.pump_x = SmarActRecord(self.IdSA+'-ESB1')
-#         except:
-#             print('No Smaract THzZ')
-#             pass
-# 
-#         try:
-#             self.par_x = SmarActRecord(self.IdSA+'-ESB5')
-#         except:
-#             print('No Smaract ParX')
-#             pass
-#         try:
-#             self.par_z = SmarActRecord(self.IdSA+'-ESB4')
-#         except:
-#             print('No Smaract ParZ')
-#             pass
-
-
-    def get_adjustable_positions_str(self):
+	def get_adjustable_positions_str(self):
         ostr = '*****Motor positions******\n'
 
         for tkey,item in self.__dict__.items():
@@ -96,17 +28,7 @@ class Laser_Exp:
                 pos = item.get_current_value()
                 ostr += '  ' + tkey.ljust(10) + ' : % 14g\n'%pos
         return ostr
-                
 
-
-    #def pos(self):
-    #    s = []
-    #    for i in sorted(self.__dict__.keys()):
-    #        s.append[i]
-    #    for n, mo^tor in enumerate (s):
-    #        s[n] += ':  ' + str(self.__dict__[motor])
-    #    return s
-
-    def __repr__(self):
+	def __repr__(self):
         return self.get_adjustable_positions_str()
         
