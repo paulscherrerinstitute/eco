9f67070370da8579ebb16845d6a70a09dba925b7
Chris Milne <cjmilne@gmail.com>
2018-06-29 19:57:04 +0200

    typos

--- loptics/alvra_laser_shutter.py
+++ loptics/alvra_laser_shutter.py
@@ -2,28 +2,28 @@ from epics import caput, caget
 
 class laser_shutter:
 	def __init__(self,Id):
-        self.Id = Id
+		self.Id = Id
 
 	def __repr__(self):
-        return self.get_status()
+		return self.get_status()
     
 	def get_status(self):
-        Id = self.Id
-        status = caget(Id+":FrontUnivOut5_Ena-SP")
-        if status == 0:
-            return 'open'
-        elif status == 1:
-            return 'close'
-        else:
-            return "unknown"
+		Id = self.Id
+		status = caget(Id+":FrontUnivOut5_Ena-SP")
+		if status == 0:
+			return 'open'
+		elif status == 1:
+			return 'close'
+		else:
+			return "unknown"
 
 	def open(self):
-		caput(self.Id+":FrontUnivOut5_SOURCE",4)
-		caput(self.Id+":FrontUnivOut5_SOURCE2",3)
+		caput(self.Id+":FrontUnivOut5_SOURCE",3)
+		caput(self.Id+":FrontUnivOut5_SOURCE2",4)
 		caput(self.Id+":FrontUnivOut5-Ena-SP",0)
 
 	def close(self):
-		caput(self.Id+":FrontUnivOut5_SOURCE",4)
-		caput(self.Id+":FrontUnivOut5_SOURCE2",3)
+		caput(self.Id+":FrontUnivOut5_SOURCE",3)
+		caput(self.Id+":FrontUnivOut5_SOURCE2",4)
 		caput(self.Id+":FrontUnivOut5-Ena-SP",1)
 		
