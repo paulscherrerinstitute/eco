5bfb89c1ac27dd3ed465de38c0c106bb68640691
Chris Milne <cjmilne@gmail.com>
2018-06-29 19:50:36 +0200

    tabs Vs spaces who ya got ?

--- loptics/alvra_laser_shutter.py
+++ loptics/alvra_laser_shutter.py
@@ -1,13 +1,13 @@
 from epics import caput, caget
 
 class laser_shutter:
-    def __init__(self,Id):
+	def __init__(self,Id):
         self.Id = Id
 
-    def __repr__(self):
+	def __repr__(self):
         return self.get_status()
     
-    def get_status(self):
+	def get_status(self):
         Id = self.Id
         status = caget(Id+":FrontUnivOut5_Ena-SP")
         if status == 0:
@@ -17,13 +17,13 @@ class laser_shutter:
         else:
             return "unknown"
 
-    def open(self):
-        caput(self.Id+":FrontUnivOut5_SOURCE",4)
-        caput(self.Id+":FrontUnivOut5_SOURCE2",3)
-        caput(self.Id+":FrontUnivOut5-Ena-SP",0)
+	def open(self):
+		caput(self.Id+":FrontUnivOut5_SOURCE",4)
+		caput(self.Id+":FrontUnivOut5_SOURCE2",3)
+		caput(self.Id+":FrontUnivOut5-Ena-SP",0)
 
-    def close(self):
-        caput(self.Id+":FrontUnivOut5_SOURCE",4)
-        caput(self.Id+":FrontUnivOut5_SOURCE2",3)
+	def close(self):
+		caput(self.Id+":FrontUnivOut5_SOURCE",4)
+		caput(self.Id+":FrontUnivOut5_SOURCE2",3)
 		caput(self.Id+":FrontUnivOut5-Ena-SP",1)
 		
