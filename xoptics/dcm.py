fb9d52d654b0763bd101db4851403367e8e905d0
Chris Milne <cjmilne@gmail.com>
2018-06-30 22:55:20 +0200

    updated monoFEL to Niko PVs

--- xoptics/dcm.py
+++ xoptics/dcm.py
@@ -161,51 +161,67 @@ class AlvraDCM_FEL:
 	def __init__(self,Id):
 		self.Id = Id
 		self.name = 'Alvra DCM monochromator coupled to FEL beam'
-		self.IOCstatus = PV('ALVRA:running')			# bool 0 running, 1 not running
-		self.FELcoupling = PV('ALVRA:SetIDON')			# string "Off" or "e-beam"
-		self.setEnergy = PV('ALVRA:SetEnergy')			# float eV
-		self.getEnergy = PV('SAROP11-ARAMIS:ENERGY')	# float eV
-		self.ebeamEnergy = PV('SARCL02-MBND100:P-READ')	# float MeV/c
-		self.ebeamEnergySP = PV('ALVRA:Energy_SP')		# float MeV
-		self.dcmStop = PV('SAROP11-ODCM105:STOP.PROC')	# stop the DCM motors
-		self.dcmMoving = PV('SAROP11-ODCM105:MOVING')	# DCM moving field
-		self.alvraMode = PV('SAROP11-ARAMIS:MODE')		# string Aramis SAROP11 mode
+#		self.IOCstatus = PV('ALVRA:running')					# bool 0 running, 1 not running
+		self.FELcoupling = PV('SGE-OP2E-ARAMIS:MODE_SP')		# string "Off" or "e-beam"
+		self.setEnergy = PV('SAROP11-ARAMIS:ENERGY_SP_USER')	# float eV
+		self.getEnergy = PV('SAROP11-ARAMIS:ENERGY')			# float eV
+		self.ebeamEnergy = PV('SARCL02-MBND100:P-READ')			# float MeV/c
+#		self.ebeamEnergySP = PV('ALVRA:Energy_SP')				# float MeV
+		self.dcmStop = PV('SAROP11-ODCM105:STOP.PROC')			# stop the DCM motors
+		self.dcmMoving = PV('SAROP11-ODCM105:MOVING')			# DCM moving field
+		self.energyChanging = PV('SGE-OP2E-ARAMIS:MOVING')		# PV telling you something related to the energy is changing
+		self.alvraMode = PV('SAROP11-ARAMIS:MODE')				# string Aramis SAROP11 mode
 		self.ebeamOK = PV('SFB_BEAM_ENERGY_ECOL:SUM-ERROR-OK')	# is ebeam no longer changing
+		self.photCalib1 = PV('SGE-OP2E-ARAMIS:PH2E_X1')			# photon energy calibration low calibration point
+		self.photCalib2 = PV('SGE-OP2E-ARAMIS:PH2E_X2')			# photon energy calibration high calibration point
+		self.ebeamCalib1 = PV('SGE-OP2E-ARAMIS:PH2E_Y1')		# electron energy calibration low calibration point
+		self.ebeamCalib2 = PV('SGE-OP2E-ARAMIS:PH2E_Y2')		# electron energy calibration high calibration point
 		
 	def __str__(self):
 		ioc = self.IOCstatus.get()
-		if ioc == 0:
-			iocStr = "Soft IOC running"
-		else:
-			iocStr = "Soft IOC not running"
+# 		if ioc == 0:
+# 			iocStr = "Soft IOC running"
+# 		else:
+# 			iocStr = "Soft IOC not running"
 		FELcouplingStr = self.FELcoupling.get(as_string=True)
 		alvraModeStr = self.alvraMode.get(as_string=True)
 		currEnergy = self.getEnergy.get()
 		currebeamEnergy = self.ebeamEnergy.get()
+		photCalib1Str = self.photCalib1.get()
+		photCalib2Str = self.photCalib2.get()
+		ebeamCalib1Str = self.ebeamCalib1.get()
+		ebeamCalib2Str = self.ebeamCalib2.get()
 		
 		s = '**Alvra DCM-FEL status**\n\n'
 # 		print('%s'%iocStr)
 #  		print('FEL coupling %s'%FELcouplingStr)
 #  		print('Alvra beamline mode %s'%alvraModeStr)
 #  		print('Photon energy (eV) %'%currEnergy)
-		s += '%s\n'%iocStr
+# 		s += '%s\n'%iocStr
 		s += 'FEL coupling: %s\n'%FELcouplingStr
 		s += 'Alvra beamline mode: %s\n'%alvraModeStr
 		s += 'Photon energy: %.2f eV\n'%currEnergy
 		s += 'Electron energy: %.2f MeV\n'%currebeamEnergy
+		s += 'Calibration set points:\n'
+		s += 'Low: Photon %.2f, Electron %.2f'%(photCalib1Str, ebeamCalib1Str)
+		s += 'High: Photon %.2f, Electron %.2f'%(photCalib2Str, ebeamCalib2Str)
 		return s
 		
 	def get_current_value(self):
 		return self.getEnergy.get()
 		
 	def move_and_wait(self,value,checktime=.1,precision=0.5):
+		self.FELcoupling.put(1)			# ensure the FEL coupling is turned on
 		self.setEnergy.put(value)
-		while self.ebeamOK.get()==0:
+# 		while self.ebeamOK.get()==0:
+# 			sleep(checktime)
+# 		while abs(self.ebeamEnergy.get()-self.ebeamEnergySP.get())>precision:
+# 			sleep(checktime)
+# 		while self.dcmMoving.get()==1:
+# 			sleep(checktime)
+		while self.energyChanging == 1
 			sleep(checktime)
-		while abs(self.ebeamEnergy.get()-self.ebeamEnergySP.get())>precision:
-			sleep(checktime)
-		while self.dcmMoving.get()==1:
-			sleep(checktime)
+
 
 	def changeTo(self,value,hold=False):
 		changer = lambda value: self.move_and_wait(value)
