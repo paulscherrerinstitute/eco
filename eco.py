641b31687daca82ee2d295fdb3a1c4994c9877aa
Simon Ebner <simon.ebner@psi.ch>
2018-07-06 11:44:02 +0200

    refactor

--- eco.py
+++
@@ -1,6 +0,0 @@
-from epics import Motor as _Motor_pyepics
-
-class Motor:
-    def __init__(PV):
-        pass
-        
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+VERSION = (0, 0, 1)
+VERSION_STR = ".".join([str(x) for x in VERSION])
+
+setup(
+    name='eco',
+    version=VERSION_STR,
+    description="Eco ...",
+    long_description="Eco ../",
+    author='Paul Scherrer Institute',
+    author_email='@psi.ch',
+    url='https://github.com/paulscherrerinstitute/eco',
+    packages=find_packages(),
+    requires=['numpy'],
+
+)
