f0251941efad89ddeb0966270690ad703e919f70
Henrik Lemke <henrik.lemke@psi.ch>
2018-09-23 11:24:39 +0200

    bernina, 1st transition to new config

---
+++ eco/ecocnf.py
@@ -0,0 +1,16 @@
+
+startup_lazy = False
+
+scopes = [
+        {
+            'name'      :'Alvra',
+            'facility'  :'SwissFEL',
+            'module'    :'alvra'
+            },
+        {
+            'name'      :'Bernina',
+            'facility'  :'SwissFEL',
+            'module'    :'bernina'
+            }
+        ]
+
