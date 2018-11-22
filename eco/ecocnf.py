9e7d92b928b1fbf2d63553e6ced6b3f59dd5da94
Roman Mankowsky <roman.mankowsky@psi.ch>
2018-11-22 17:20:27 +0100

    started addind swissmx

--- eco/ecocnf.py
+++ eco/ecocnf.py
@@ -3,4 +3,5 @@ startup_lazy = False
 scopes = [
     {"name": "Alvra", "facility": "SwissFEL", "module": "alvra"},
     {"name": "Bernina", "facility": "SwissFEL", "module": "bernina"},
+    {"name": "SwissMX", "facility": "SwissFEL", "module": "swissmx"},
 ]
