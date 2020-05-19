07804212858da6dcc577d01ab6635eeab44b3d9c
Roman Mankowsky <roman.mankowsky@psi.ch>
2020-05-19 17:55:24 +0200

    added symmetrie hexapod start

--- eco/endstations/hexapod.py
+++ eco/endstations/hexapod.py
@@ -1,3 +1,7 @@
+from epics import PV
+from ..devices_general.adjustable import PvEnum
+from time import sleep
+
 class Hexapod_PI:
     def __init__(self, Id):
         self.Id = Id
@@ -13,3 +17,81 @@ class Hexapod_PI:
             ValueRdback(self.id + f":SET-PIVOT-{i}", self.id + f":PIVOT-R-{i}")
             for i in "RST"
         ]
+
+
+class HexapodSymmetrie:
+    def __init__(self,pv_master='SARES20-HEXSYM',name='hex_usd'):
+        self.name = name
+        self.pvname = ps_master
+        self.coordinate_switch = PvEnum(f'{self.pvname}:MOVE#PARAM:CM',name='hex_usd_coordinate_switch')
+        self.pvs_setpos = {
+                'x':PV('{self.pvname}:MOVE#PARAM:X.VAL'),
+                'y':PV('{self.pvname}:MOVE#PARAM:Y.VAL'),
+                'z':PV('{self.pvname}:MOVE#PARAM:Z.VAL'),
+                'rx':PV('{self.pvname}:MOVE#PARAM:RX.VAL'),
+                'ry':PV('{self.pvname}:MOVE#PARAM:RY.VAL'),
+                'rz':PV('{self.pvname}:MOVE#PARAM:RZ.VAL'),
+                }
+        self.pvs_getpos = {
+                'x':PV('{self.pvname}:POSMACH.X'),
+                'y':PV('{self.pvname}:POSMACH.Y'),
+                'z':PV('{self.pvname}:POSMACH.Z'),
+                'rx':PV('{self.pvname}:POSMACH.RX'),
+                'ry':PV('{self.pvname}:POSMACH.RY'),
+                'rz':PV('{self.pvname}:POSMACH.RZ'),
+                }
+        self._ctrl_pv = f'{self.pvname}:STATE#PANEL:SET.VAL')
+
+    def set_coordinates(self,x,y,z,rx,ry,rz):
+        self.pvs_setpos['x'].set(x)
+        self.pvs_setpos['y'].set(y)
+        self.pvs_setpos['z'].set(z)
+        self.pvs_setpos['rx'].set(rx)
+        self.pvs_setpos['ry'].set(ry)
+        self.pvs_setpos['rz'].set(rz)
+    
+    def get_coordinates(self,x,y,z,rx,ry,rz):
+        x = self.pvs_getpos['x'].get()
+        y = self.pvs_getpos['y'].get()
+        z = self.pvs_getpos['z'].get()
+        rx = self.pvs_getpos['rx'].get()
+        ry = self.pvs_getpos['ry'].get()
+        rz = self.pvs_getpos['rz'].get()
+        return x,y,z,rx,ry,rz
+
+    def set_control_on(self):
+        self._ctrl_pv.set(3)
+
+    def set_control_off(self):
+        self._ctrl_pv.set(4)
+
+    def get_control_state(self):
+        stat = self._ctrl_pv.get()
+        if stat==3:
+            return 'control on'
+        elif stat==4:
+            return 'control on'
+        elif stat==2:
+            return 'stopped'
+        elif stat==11:
+            return 'moving'
+
+    def start_move(self,target=None,precision=[.001,.001,.001,.001,.001,.001]):
+        print('Starting to move... stop with Ctrl-C')
+        self._ctrl_pv.set(11)
+        while 1:
+            try:
+                if target:
+                    coo = self.get_coordinates()
+                    if all([abs(ctarg-cnow)<cprec for ctarg,cnow,cprec in zip(target,coo,precision)]):
+                        self.stop_move()
+                        print('Target position reached')
+                        break
+                sleep(.01) 
+            except KeyboardInterrupt:
+                self.stop_move()
+                print('Motion stopped')
+
+    def stop_move(self):
+        self._ctrl_pv.set(2)
+
