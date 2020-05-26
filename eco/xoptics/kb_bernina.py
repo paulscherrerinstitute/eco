e438dd736d2b2548998052c10cef40f5040189d0
Roman Mankowsky <roman.mankowsky@psi.ch>
2020-05-26 22:58:48 +0200

    added kb calculations start

---
+++ eco/xoptics/kb_bernina.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy import constants
+
+class KBMirrorBernina:
+    def __init__(self,d_kbver=3350.,d_kbhor=2600.,d_hex=1600.,d_win1=1945.,d_win2=1330.,d_target=1520.,d_att=1420.):
+        """All distances are from sample interaction point at straight beam (no kb deflection), the units are expected in mm"""
+        self.d_kbver = d_kbver
+        self.d_kbhor = d_kbhor
+        self.d_hex = d_hex
+        self.d_att = d_att
+        self.d_win2 = d_win2
+        self.d_win1 = d_win1
+        self.d_target = d_target
+
+    def calc_positions(self,the_kbver,the_kbhor):
+        """angles in rad"""
+        y_kbhor = np.tan(2*the_kbver) * np.abs(self.d_kbver-self.d_kbhor)
+        rx_kbhor = - 2 * the_kbver
+        y_hex = np.tan(2*the_kbver) * np.abs(self.d_kbver-self.d_hex)
+        x_hex = np.tan(2*the_kbhor) * np.abs(self.d_kbhor-self.d_hex)
+        rx_hex = rx_kbhor
+        ry_hex = 2* the_kbhor
+        return {'y_kbhor':y_kbhor,'rx_kbhor':rx_kbhor,'x_hex':x_hex,'y_hex':y_hex,'rx_hex':rx_hex,'ry_hex':ry_hex}
+
+    def calc_fwhm(self,fwhm_hor,fwhm_ver,z_focver=0,z_fochor=0,E_phot=None):
+        """ E_phot in eV, length units in mm."""
+        lam = constants.c*constants.h/constants.electron_volt /E_phot
+        print(lam*1e10)
+        fwhm_fac = 1/np.sqrt(2*np.log(2))# w = fwhm_fac*fwhm
+        # div_ver = np.arctan(fwhm_ver*fwhm_fac/2/(self.d_kbver+z_focver)) #half divergence
+        # div_hor = np.arctan(fwhm_hor*fwhm_fac/2/(self.d_kbhor+z_fochor))
+        c = lam/np.pi*1e3
+
+        w0 = lambda w,z:( w**2 - ( w**4 - (2*c*z)**2 )**0.5)**0.5/np.sqrt(2)
+        w = lambda w0,z: (w0**2+(c*z/w0)**2)**0.5
+        zr = lambda w0: w0**2/c
+
+        fwhm_z = lambda z,w0: w(w0,z)/fwhm_fac
+        w0_hor = w0(fwhm_hor*fwhm_fac,self.d_kbhor+z_fochor)
+        w0_ver = w0(fwhm_ver*fwhm_fac,self.d_kbver+z_focver)
+        print(w0_hor)
+        res = {}
+        res['target'] = (fwhm_z(self.d_target+z_fochor,w0_hor),fwhm_z(self.d_target+z_focver,w0_ver))
+        res['win1'] = (fwhm_z(self.d_win1+z_fochor,w0_hor),fwhm_z(self.d_win1+z_focver,w0_ver))
+        res['win2'] = (fwhm_z(self.d_win2+z_fochor,w0_hor),fwhm_z(self.d_win2+z_focver,w0_ver))
+        res['att'] = (fwhm_z(self.d_att+z_fochor,w0_hor),fwhm_z(self.d_att+z_focver,w0_ver))
+        res['sample'] = (fwhm_z(z_fochor,w0_hor),fwhm_z(z_focver,w0_ver))
+        # res['fwhm_kbver'] = (fwhm_z(self.d_kbver+z_fochor,w0_hor),fwhm_z(self.d_kbver+z_focver,w0_ver))
+        # res['fwhm_kbhor'] = (fwhm_z(self.d_kbhor+z_fochor,w0_hor),fwhm_z(self.d_kbhor+z_focver,w0_ver))
+        return res
+
+
+
+       
