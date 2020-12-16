83ec86482b52f29898aefc133246bb967b542177
Roman Mankowsky <roman.mankowsky@psi.ch>
2020-12-16 15:21:25 +0100

    added kappa computing

---
+++ eco/endstations/kappa_conversion.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def you2kappa(eta, chi, phi, kappa_angle=60, degrees=True):
+    """tool to convert from you definition angles to kappa angles, in
+    particular the bernina kappa where the"""
+    if degrees:
+        eta, chi, phi, kappa_angle = np.deg2rad([eta, chi, phi, kappa_angle])
+    delta_angle = np.arcsin(-np.tan(chi / 2) / np.tan(kappa_angle))
+    eta_k = eta - delta_angle
+    kappa = 2 * np.arcsin(np.sin(chi / 2) / np.sin(kappa_angle))
+    # phi_k = phi - delta_angle
+
+    eta_k = -np.pi / 2 + eta_k
+    if degrees:
+        eta_k, kappa, phi_k = np.rad2deg([eta_k, kappa, phi_k])
+    return eta_k, kappa, phi_k
+
+
+def kappa2you(eta_k, kappa, phi_k, kappa_angle=60, degrees=True):
+    if degrees:
+        eta_k, kappa, phi_k, kappa_angle = np.deg2rad(
+            [eta_k, kappa, phi_k, kappa_angle]
+        )
+    eta_k = np.pi / 2 + eta_k
+    delta_angle = np.arctan(np.tan(kappa / 2) * np.cos(kappa_angle))
+    eta = eta_k - delta_angle
+    chi = 2 * np.arcsin(np.sin(kappa / 2) * np.sin(kappa_angle))
+    # phi = phi_k - delta_angle
+    if degrees:
+        eta, chi, phi = np.rad2deg([eta, chi, phi])
+    return eta, chi, phi
---
+++ eco/xoptics/kb_mirrors.py
@@ -0,0 +1,71 @@
+from ..elements.assembly import Assembly
+from ..devices_general.motors import MotorRecord
+from ..devices_general.adjustable import PvRecord, PvEnum
+
+
+class KbVer(Assembly):
+    def __init__(self, pvname, name=None):
+        self.pvname = pvname
+        super().__init__(name=name)
+        self._append(MotorRecord, pvname + ":W_X", name="x", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_Y", name="y", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RX", name="pitch", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RZ", name="roll", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RY", name="yaw", is_setting=True)
+        self._append(MotorRecord, pvname + ":BU", name="bend1", is_setting=True)
+        self._append(MotorRecord, pvname + ":BD", name="bend2", is_setting=True)
+        self._append(
+            PvRecord,
+            pvname + ":CURV_SP",
+            pvreadbackname=pvname + ":CURV",
+            accuracy=0.002,
+            name="curv",
+        )
+        self._append(
+            PvRecord,
+            pvname + ":ASYMMETRY_SP",
+            pvreadbackname=pvname + ":CURV",
+            accuracy=0.002,
+            name="asym",
+        )
+
+        #### actual motors ###
+        self._append(MotorRecord, pvname + ":TY1", name="_Y1", is_setting=False)
+        self._append(MotorRecord, pvname + ":TY2", name="_Y2", is_setting=False)
+        self._append(MotorRecord, pvname + ":TY3", name="_Y3", is_setting=False)
+        self._append(MotorRecord, pvname + ":TX1", name="_X1", is_setting=False)
+        self._append(MotorRecord, pvname + ":TX2", name="_X2", is_setting=False)
+
+
+class KbHor(Assembly):
+    def __init__(self, pvname, name=None):
+        self.pvname = pvname
+        super().__init__(name=name)
+        self._append(MotorRecord, pvname + ":W_X", name="x", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_Y", name="y", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RY", name="pitch", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RZ", name="roll", is_setting=True)
+        self._append(MotorRecord, pvname + ":W_RX", name="yaw", is_setting=True)
+        self._append(MotorRecord, pvname + ":BU", name="bend1", is_setting=True)
+        self._append(MotorRecord, pvname + ":BD", name="bend2", is_setting=True)
+        self._append(
+            PvRecord,
+            pvname + ":CURV_SP",
+            pvreadbackname=pvname + ":CURV",
+            accuracy=0.002,
+            name="curv",
+        )
+        self._append(
+            PvRecord,
+            pvname + ":ASYMMETRY_SP",
+            pvreadbackname=pvname + ":CURV",
+            accuracy=0.002,
+            name="asym",
+        )
+
+        #### actual motors ###
+        self._append(MotorRecord, pvname + ":TY1", name="_Y1", is_setting=False)
+        self._append(MotorRecord, pvname + ":TY2", name="_Y2", is_setting=False)
+        self._append(MotorRecord, pvname + ":TY3", name="_Y3", is_setting=False)
+        self._append(MotorRecord, pvname + ":TX1", name="_X1", is_setting=False)
+        self._append(MotorRecord, pvname + ":TX2", name="_X2", is_setting=False)
