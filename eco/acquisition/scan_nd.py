0d7f4f98a2e29e782c07bc7f919d77ef6ba5e1f1
Roman Mankowsky <roman.mankowsky@psi.ch>
2021-02-24 17:09:50 +0100

    continue work on nd scan

--- eco/acquisition/scan_nd.py
+++ eco/acquisition/scan_nd.py
@@ -41,4 +41,22 @@ class ScanND:
         run_table=None,
         elog=None,
     ):
-        pass
+        scan_array = []
+        scan_adjustables = []
+        for n_dim, (adj_tdim, arr_tdim) in enumerate(zip(adjustables, arrays)):
+            # check if the dimension is a multi adjustable thing
+            try:
+                iter(adj_tdim)
+                if not len(adj_tdim) == len(arr_tdim):
+                    raise Exception(
+                        f"arrays and adjusbles in dimension {n_dim}don't match"
+                    )
+                if len(set([len(sarr) for sarr in arr_tdim])) > 1:
+                    raise Exception(
+                        f"subarrays in dimension {n_dim} do not have equal length!"
+                    )
+                scan_array.append(tuple(arr_tdim))
+                scan_adjustables.append(tuple(adj_tdim))
+            except TypeError:
+                scan_array.append(tuple([arr_tdim]))
+                scan_adjustables.append(tuple([adj_tdim]))
