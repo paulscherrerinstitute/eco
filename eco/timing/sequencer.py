0e0a942eb464b5c93fa89a8ff77de5dd7be85740
Mathias Sander <mathias.sander@psi.ch>
2021-10-11 20:43:00 +0200

    additions to motors, path_alias, bugfox in scan

--- eco/timing/sequencer.py
+++ eco/timing/sequencer.py
@@ -1,4 +1,4 @@
-from ..epics import AdjustablePv
+from ..epics.adjustable import AdjustablePv,AdjustablePvEnum
 from ..elements import Assembly
 
 class CtaSequencer(Assembly):
@@ -6,8 +6,16 @@ class CtaSequencer(Assembly):
         super().__init__(name=name)
         self.pvname = pvname
         self.sequence_number = sequence_number
+        self._append(AdjustablePv,self._pvstr('SerMaxLen-0'),name='max_length',is_setting=True)
         self._append(AdjustablePv,self._pvstr('Crtl-Length-I'),name='length',is_setting=True)
-        self._append(AdjustablePv,self._pvstr('Crtl-Cycles-I'),name='length',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Crtl-Cycles-I'),name='cycles',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-SCfgMode-I'),name='mode',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-SCfgModDivisor-I'),name='start_condition_pulse_id_divisor',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-SCfgModOffset-I'),name='start_condition_pulse_id_offset',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-Start-I'),name='start',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-Stop-I'),name='stop',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-IsRunning-O'),name='is_running',is_setting=True)
+        self._append(AdjustablePv,self._pvstr('Ctrl-StartedAt-O'),name='start_pulse_id',is_setting=True)
 
     def _pvstr(self,suffix=''):
         return f'{self.pvname}:seq{self.sequence_number:d}{suffix}'
